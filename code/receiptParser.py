65a0a194235515dba556e744490a7c03bc88177a
Avishek Paul <avishek97paul@gmail.com>
2018-10-08 01:08:38 -0400

    MVP of receiptTracker + added some more receipts to test with

--- code/receiptParser.py
+++ code/receiptParser.py
@@ -7,10 +7,30 @@ class receiptParser:
 	raw_tickets = None
 
 	def __init__(self, image_folder_path):
-		self.image_files = [f for f in os.listdir(image_folder_path) if os.path.isfile(os.path.join(image_folder_path, f))] 
-		
+		self.image_files = [f for f in os.listdir(image_folder_path) if bool(os.path.isfile(os.path.join(image_folder_path, f)) and '.jpg' in f)] 
+		print(self.image_files)
+
 	def scrape_tickets(self):
 		self.raw_tickets = self.ocr(self.image_files)
+
+	def format_num(self, num):
+		return num.replace(',','.').replace('-','.')
+
+	def determine_num(self, num):
+		#input: string
+		#output: boolean
+		num = self.format_num(num)
+		
+		if num.count('.')==1:
+			vals = num.split('.')# if '.' in num else num.split(',')
+			if ('$' in num):
+				return True #on basis of it being in the form $XX.YY
+			elif len(vals[1])==2 and vals[0].isdigit() and vals[1].isdigit(): #In form XX.YY
+				return True
+			else:
+				return False
+		else:
+			return False 	 
 
 	def parse_tickets(self):
 		if not self.raw_tickets:
@@ -19,10 +39,17 @@ class receiptParser:
 		count = 1
 		for raw_ticket in self.raw_tickets:
 			ticket_data = raw_ticket.split()
-			nums = [float(num[1:]) for num in ticket_data if bool('$' in num and '.' in num)]
-			total = max(nums)
+			
+			nums = [float(self.format_num(num.replace('$',''))) for num in ticket_data if self.determine_num(num)]			
+
+			if nums:
+				total = max(nums)
+			else:
+				total = "[UNDEFINED]"
+
 			print("The total cost of receipt #{} is: ${}".format(count,total))
-			count+=1	
+			count+=1
+				
 
 if __name__=="__main__":
 	rp = receiptParser('images/')
